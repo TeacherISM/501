1f478b67a2209c3a9e72c96a7fc86ea85bc8e27c
A01352283 <a01352283@tec.mx>
2023-06-08 18:35:48 -0600

    Retries restructuring

--- ABC/src/app.py
+++ ABC/src/app.py
@@ -1,18 +1,2 @@
-import json
-
-
-def lambda_handler(event, context):
-    # Extract the numbers from the event payload
-    number1 = event['number1']
-    number2 = event['number2']
-
-    # Perform the addition
-    result = number1 + number2
-
-    # Prepare the response
-    response = {
-        'statusCode': 200,
-        'body': json.dumps({'sum': result})
-    }
-
-    return response
+def handler(event, context):
+    return "Hello, world! A01352283"
--- ABC/test/test_app.py
+++ ABC/test/test_app.py
@@ -1,20 +1,7 @@
-import json
-from src import lambda_handler
-
+# test_hello_world.py
+import pytest
+from src import app
 
 def test_lambda_handler():
-    # Define the test input
-    event = {
-        'number1': 3,
-        'number2': 5
-    }
-
-    # Invoke the lambda_handler
-    result = lambda_handler(event, None)
-
-    # Extract the response body
-    response_body = json.loads(result['body'])
-
-    # Assert the expected values
-    assert result['statusCode'] == 200
-    assert response_body['sum'] == 8
+    response = app.handler(None, None)
+    assert response == "Hello, world! A01352283"
