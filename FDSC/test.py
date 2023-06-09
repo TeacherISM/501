1a9f0c9409b5484fdcfeda8eae895efed1431c55
f-salcedo-c <a01633010@tec.mx>
2023-06-08 19:42:31 -0600

    dict fix

--- FDSC/test.py
+++ FDSC/test.py
@@ -4,13 +4,13 @@ from app import lambda_handler
 
 def test_simplesum():
 
-	assert lambda_handler({number_one:1, number_two:2},{}) == 3
+	assert lambda_handler({"number_one":1, "number_two":2},{}) == 3
 
 def test_badsum():
-	assert lambda_handler({number_one:"1", number_two:2},{})==None
+	assert lambda_handler({"number_one":"1", "number_two":2},{})==None
 
 def test_floatsum():
-	assert number_sum({number_one:1.5, number_two:1.5},{})==None
+	assert number_sum({"number_one":1.5, "number_two":1.5},{})==None
 
 if __name__ == '__main__':
     pytest.main()
