aa9047d5eb5d3586c2d1d2f5fc05e81b3e426400
f-salcedo-c <a01633010@tec.mx>
2023-06-08 19:43:42 -0600

    logic fix

--- FDSC/app.py
+++ FDSC/app.py
@@ -11,16 +11,11 @@ def lambda_handler(event, context):
     """
     Code for a lambda aws handler to sum two numbers.
     """
-    
+
     number_one = event['number_one']
     number_two = event['number_two']
 
     if isinstance(number_one, int) and isinstance(number_two, int):
-        return {
-            'sum': number_one + number_two
-        }
+        return number_one + number_two
     else:
-        return {
-            'error': 'Invalid input'
-        }
-
+        return None
--- FDSC/test.py
+++ FDSC/test.py
@@ -10,7 +10,7 @@ def test_badsum():
 	assert lambda_handler({"number_one":"1", "number_two":2},{})==None
 
 def test_floatsum():
-	assert number_sum({"number_one":1.5, "number_two":1.5},{})==None
+	assert lambda_handler({"number_one":1.5, "number_two":1.5},{})==None
 
 if __name__ == '__main__':
     pytest.main()
