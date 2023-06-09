81d5aec8bfa0fdfbbd38d751f8e992f6e0ba2583
SeisshinKB <75494927+ivalani@users.noreply.github.com>
2023-06-08 20:34:41 -0600

    :(

--- python3/test/test_lamba.py
+++ python3/test/test_lamba.py
@@ -1,8 +1,7 @@
-#from src import app
-from app import lambda_handler
+from src import app
 from unittest import TestCase
 import sys
-sys.path.append('../src')
+sys.path.append('..')
 
 
 class AppTest(TestCase):
@@ -10,7 +9,7 @@ class AppTest(TestCase):
         event = {'numbers': [1, 2, 3, 4, 5]}
         expected_result = 15
 
-        response = lambda_handler(event, None)
+        response = app.lambda_handler(event, None)
 
         self.assertEqual(response['statusCode'], 200)
         self.assertEqual(response['body'], expected_result)
