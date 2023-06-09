5e840bc24745fde11f0db9e49f864c2efd68a97a
Ace0x <59582360+Ace0x@users.noreply.github.com>
2023-06-08 19:30:46 -0600

    new flow

--- ADV/src/app.py
+++ ADV/src/app.py
@@ -1,11 +1,14 @@
+import logging
+
 def AWS_Lambda(event, context):
     a = 1
     b = 2
     
-    print(a+b)
+    logging.info(a + b)
+    
     return {
         'statusCode': 200,
         'body': a + b
     }
-    
+
     
--- ADV/test/test_lambda.py
+++ ADV/test/test_lambda.py
@@ -1,10 +1,21 @@
 import sys
 import os
+import logging
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.app import AWS_Lambda as handler
 
-def test_handler():
+def test_handler(caplog):
+    caplog.set_level(logging.INFO)  # Set the log level to capture INFO and above
+
     ret = handler(None, None)
+
     assert ret['statusCode'] == 200
     assert ret['body'] == 3
+
+    # Retrieve the captured logs
+    logs = [record.getMessage() for record in caplog.records]
+
+    # Assert that the expected log message is present
+    assert '3' in logs
