8e35cef936bfb462be8a40a99d4f09f6eb06ca74
SeisshinKB <75494927+ivalani@users.noreply.github.com>
2023-06-08 21:02:49 -0600

    self

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -6,7 +6,9 @@ sys.path.append('../src')
 
 
 class AppTest(TestCase):
-    def test_handler():
+    def test_handler(self):
         result = app.lambda_handler(None, None)
         assert result['statusCode'] == 200
         assert result['body'] == 18
+
+
