743926c3e0df23b5502686afb572f9c7296153a9
Diego-CP <pinedo.dc@gmail.com>
2023-06-08 18:28:15 -0600

    Fixed linting I think, again, again

--- DCP/tests/test_app.py
+++ DCP/tests/test_app.py
@@ -1,9 +1,8 @@
 from src import app
-import pytest
 import sys
 sys.path.append('../src')
 
 
 def test_lambda_handler():
-    result = app.lambda_handler(None , None)
+    result = app.lambda_handler(None, None)
     assert result == 4
