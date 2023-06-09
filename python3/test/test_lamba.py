101bb2e53351d7f853071061dab09dcf0a51a99c
SeisshinKB <75494927+ivalani@users.noreply.github.com>
2023-06-08 20:28:48 -0600

    test

--- python3/test/test_lamba.py
+++ python3/test/test_lamba.py
@@ -5,10 +5,6 @@ sys.path.append('../src')
 
 
 class AppTest(TestCase):
-    def test_handler(self):
-        result = app.lambda_handler(None, None)
-        self.assertEqual(result, 4)
-
     def test_sum_numbers(self):
         event = {'numbers': [1, 2, 3, 4, 5]}
         expected_result = 15
