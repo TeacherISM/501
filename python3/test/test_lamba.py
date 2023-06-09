c1e3727014b8e6e0e7765a9d3e6c74d6633e3332
SeisshinKB <75494927+ivalani@users.noreply.github.com>
2023-06-08 20:15:44 -0600

    test

--- python3/test/test_lamba.py
+++ python3/test/test_lamba.py
@@ -1,4 +1,4 @@
-from src import lambda_handler
+from src import app
 from unittest import TestCase
 import sys
 sys.path.append('../src')
@@ -6,14 +6,15 @@ sys.path.append('../src')
 
 class AppTest(TestCase):
     def test_handler(self):
-        result = lambda_handler(None , None)
+        result = app.lambda_handler(None , None)
         self.assertEqual(result, 4)
 
-def test_sum_numbers():
+class AppTest(TestCase): 
+    def test_sum_numbers():
     event = {'numbers': [1, 2, 3, 4, 5]}
     expected_result = 15
 
-    response = lambda_handler(event, None)
+    response = app.ambda_handler(event, None)
 
     assert response['statusCode'] == 200
     assert response['body'] == expected_result
