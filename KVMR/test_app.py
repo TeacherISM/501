b19e32cf68afddb7d92b74ae465234e7a22b7ec4
karla-mond <karla.mondragon.rosas@gmail.com>
2023-06-08 19:26:40 -0600

    print en test_app

--- KVMR/test_app.py
+++ KVMR/test_app.py
@@ -6,5 +6,8 @@ def test_handler():
     context = {}
     response = app.handler(event, context)
 
+    print("Status Code:", response['statusCode'])
+    print("Body:", response['body'])
+
     assert response['statusCode'] == 200
     assert response['body'] == '55.5'
