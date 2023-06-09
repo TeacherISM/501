f4adb1e3514380cfa55b0215292e9f9703d1043f
f-salcedo-c <a01633010@tec.mx>
2023-06-08 18:35:29 -0600

    Create wheel setup and flow

---
+++ FDSC/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='app',
+      version='1.0',
+      description='Exam Build',
+      author='Francisco Salcedo',
+      author_email='a01633010@tec.mx',
+      url='',
+      packages=[],
+     )
