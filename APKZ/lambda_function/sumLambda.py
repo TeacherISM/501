edafae2a850149c9d829ad142ea0e71b3e0c7782
Ana Paula Katsuda <a01025303@tec.mx>
2023-06-08 18:47:16 -0600

    Corrected dockerfile and added print to sum lambda

--- APKZ/lambda_function/sumLambda.py
+++ APKZ/lambda_function/sumLambda.py
@@ -2,6 +2,7 @@ def lambda_handler(event, context):
     num1 = event['num1']
     num2 = event['num2']
     sum = num1 + num2
+    print("La suma es: ", sum)
     return {
         'statusCode': 200,
         'body': sum
